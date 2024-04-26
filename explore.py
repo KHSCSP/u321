cfa606db060bb604428dbea9f42b60bb3bd253ee
heyscottn <heyscottn@gmail.com>
2024-04-25 20:49:30 -0500

    mm

--- explore.py
+++ explore.py
@@ -1,7 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# ---------------------------------------------------
+# ---- intro goals:
+# 1, make a plot
+# 2, review reading from a text file and analyzing data
+# 3, use a pandas data frame to load and analyze data
+
+
+# TODO uncomment the print statements, one at a time
+# run the code each time
+
+
+# part 1 ---------------------------------------------------
 # at its most basic:
 # we create a plot, configure the options, and show it
 ages = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80]
@@ -14,7 +24,7 @@ plt.title("Just goofin")
 
 
 
-# ---------------------------------------------------
+# part 2 ---------------------------------------------------
 # using the standard file reader
 f = open("u321/temperature_data.csv")
 my_list = []
@@ -22,31 +32,31 @@ for line in f:
     temp = line.strip()
     my_list.append(temp.split(','))   # append each row as a list
 f.close()
-print("\nthe raw data: ")
+# print("\nthe raw data: ")
 # print("the list:", my_list)
-print("one row:", my_list[1])
+# print("one row:", my_list[1])
 
 # add up all 'anomalys'
 sum = 0
 for row in my_list[1:]:
     sum += float(row[1])
-print("the sum: ", sum)
+# print("the sum: ", sum)
 
 
-# ---------------------------------------------------
+# part 3---------------------------------------------------
 # using the pandas class
 temp_data = pd.read_csv("u321/temperature_data.csv", header=0)
 
-# print(temp_data)
-print("\ntype of pandas data:", type(temp_data))
-print("length:", len(temp_data))
-print("length again:", len(temp_data["Anomaly"]))
-print("one column:\n", temp_data['Anomaly'])
-print("type of column:", type(temp_data['Anomaly'])) #idk, but it is iterable!
+# print("\nthe pandas data frame\n", temp_data)
+# print("\ntype of pandas data:", type(temp_data))
+# print("length:", len(temp_data))
+# print("length again:", len(temp_data["Anomaly"]))
+# print("one column:\n", temp_data['Anomaly'])
+# print("type of column:", type(temp_data['Anomaly'])) #idk, but it is iterable!
 
 sum = 0
 for item in temp_data['Anomaly']:
     sum += item
 
-print("the sum: ", sum)
+# print("the sum: ", sum)
 
